4d0189445a76172145eb78401df21387764f7949
Saidul Hasan <SAEID271805@gmail.com>
2025-08-08 20:53:43 +0000

    Problem is not solved yet

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -324,10 +324,8 @@ const SidebarInset = React.forwardRef<
     <main
       ref={ref}
       className={cn(
-        "relative flex min-h-svh flex-1 flex-col bg-background transition-all",
-        "md:peer-data-[state=expanded]:ml-[var(--sidebar-width)]",
-        "md:peer-data-[collapsible=icon]:peer-data-[state=collapsed]:ml-[var(--sidebar-width-icon)]",
-        "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-[calc(var(--sidebar-width-icon)+theme(spacing.4))] md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
+        "flex-1 transition-all md:pl-[var(--sidebar-width)]",
+        "md:peer-data-[state=collapsed]:pl-[var(--sidebar-width-icon)]",
         className
       )}
       {...props}
